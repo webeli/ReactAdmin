d217b2d0ead1e6c6ba14e79153690a6cf0fe4ae0
Elias Hansen <pe_lias@msn.com>
2016-08-14 22:26:01 +0200

    making it work, hacky solution of using reducer, subscribe etc

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,9 @@ import { Router, Route, hashHistory } from 'react-router';
 import './../styles/App.css';
 import Projects from './Projects/Projects';
 import EditProject from './EditProject/EditProject'
-import store from '../store';
-
-store.subscribe(() => {
-    console.log("store changed", store.getState())
-});
 
 class App extends Component {
     render() {
-        console.log(store.getState());
         return (
             <Router history={hashHistory}>
                 <Route path='/' component={Projects} />
