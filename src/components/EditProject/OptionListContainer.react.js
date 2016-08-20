579bb7abe456fe62bd53aa4ec6cc96c359d20ce9
earthquaker <pe_lias@msn.com>
2016-08-20 12:09:42 +0200

    cleanup optionlistcontainer

--- src/components/EditProject/OptionListContainer.react.js
+++ src/components/EditProject/OptionListContainer.react.js
@@ -1,29 +1,13 @@
 import React, { Component } from 'react';
-import * as firebase from 'firebase';
-import { connect } from 'react-redux';
 import OptionList from './OptionList.react';
-import { getByKeys } from '../../lib/helpers';
 
 class OptionListContainer extends Component {
 
     render() {
-        if (this.props.options.key) {
-            var optionsRef = firebase.database().ref(this.props.projectKey).child('itemOptions');
-            var itemOptionsRefs = firebase.database().ref(this.props.projectKey).child('categoryItems').child(this.props.options.key).child('refs');
-            itemOptionsRefs.on('value', (snap) => {
-                this.options = getByKeys(optionsRef, snap.val());
-            }).bind(this);
-        }
-
         return (
             <OptionList projectKey={this.props.projectKey}/>
         );
     }
 }
 
-function mapStateToProps(state, ownProps) {
-    return {
-        options: state.options
-    };
-}
-export default connect(mapStateToProps)(OptionListContainer);
+export default OptionListContainer;
