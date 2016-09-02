3933738cea7a5954c14e76ad19ec7a9341d8bb01
earthquaker <pe_lias@msn.com>
2016-09-02 11:51:03 +0200

    modified the folder structure to ease my mind

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { Router, Route, browserHistory } from 'react-router';
 import './../styles/App.css';
-import Projects from './Projects/Projects';
-import EditProject from './EditProject/EditProject';
-import Login from './Login/Login';
+import Projects from './Projects/_ProjectsWrapper';
+import EditProject from './EditProject/_EditProjectWrapper';
+import Login from './Login/_LoginWrapper';
 import { syncHistoryWithStore } from 'react-router-redux';
 import store from '../store';
 
