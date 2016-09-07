f53522fd3012474dbf86bd8de741c051bde80b67
earthquaker <pe_lias@msn.com>
2016-09-07 17:17:52 +0200

    added modal to new category

--- src/components/EditProject/Project.react.js
+++ src/components/EditProject/Project.react.js
@@ -3,34 +3,43 @@ import { Grid, Row, Col, ButtonToolbar, Button } from 'react-bootstrap';
 import CategoryList from './CategoryList.react';
 import OptionList from './OptionList.react';
 import { connect } from 'react-redux';
-import ModalProjectSettings from './ModalProjectSettings';
+import ModalProjectSettings from './Modal_ProjectSettings';
+import ModalNewCategory from './Modal_NewCategory';
 import * as updateDataActions from '../../actions/updateDataActions';
 
 class Project extends Component {
 
     constructor(props){
         super(props);
-        this.state = { lgShow: false };
+        this.state = {
+            modalProjectSettings: false,
+            modalNewCategory: false
+        };
     }
 
     render() {
-        let lgClose = () => this.setState({ lgShow: false });
+        let modalProjectSettings = () => this.setState({ modalProjectSettings: false });
+        let modalNewCategory = () => this.setState({ modalNewCategory: false });
+
+
+        //<Button bsStyle="default" onClick={()=>this.props.addCategory(this.props.projectKey)}>+ Category</Button>
 
         return (
             <Grid>
-                <ModalProjectSettings show={this.state.lgShow} onHide={lgClose} />
+                <ModalProjectSettings show={this.state.modalProjectSettings} onHide={modalProjectSettings} />
+                <ModalNewCategory show={this.state.modalNewCategory} onHide={modalNewCategory} />
                 <Row className="show-grid">
                     <Col xs={12} md={12}>
                         <h2>{this.props.project.pName}</h2>
                         <p>{this.props.project.deadline}</p>
                         <ButtonToolbar>
-                            <Button onClick={()=>this.setState({ lgShow: true })}>Project Settings</Button>
+                            <Button onClick={()=>this.setState({ modalProjectSettings: true })}>Project Settings</Button>
                         </ButtonToolbar>
                         <br />
                     </Col>
                     <Col xs={12} md={6}>
                         <ButtonToolbar>
-                            <Button bsStyle="default" onClick={()=>this.props.addCategory(this.props.projectKey)}>+ Category</Button>
+                            <Button bsStyle="default" onClick={()=>this.setState({ modalNewCategory: true })}>+ Category</Button>
                         </ButtonToolbar>
                         <br />
                     </Col>
